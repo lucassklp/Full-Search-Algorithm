2e5b1d77951e155a9c8015a17799633dec5923ff
Lucas Simas <simas.lucas@hotmail.com>
2016-10-04 23:11:57 -0300

    Thread fail

--- OpenCV/Source.cpp
+++ OpenCV/Source.cpp
@@ -5,6 +5,7 @@
 #include <list>
 #include <limits>
 #include <cmath>
+#include <thread>
 
 using namespace std;
 using namespace cv;
@@ -14,89 +15,100 @@ void OpenImage(Mat image, string windowName) {
 	imshow(windowName, image);
 }	
 
-int Evaluate(Rect comparativeBlock, Rect referencialBlock, Mat Image1, Mat Image2) {
+int EvaluateSAD(Rect currentBlock, Rect referencialBlock, Mat Image1, Mat Image2) {
 	
-	int evaluationFactor = 0;
+	int SAD = 0;
 
-	for (int i = comparativeBlock.x; i < comparativeBlock.x + comparativeBlock.width; i++)
+	for (int i = currentBlock.x; i < currentBlock.x + currentBlock.width; i++)
 	{
-		for (int j = comparativeBlock.y; j < comparativeBlock.y + comparativeBlock.height; j++)
+		for (int j = currentBlock.y; j < currentBlock.y + currentBlock.height; j++)
 		{
+			int incrementX = i - currentBlock.x;
+			int incrementY = j - currentBlock.y;
+			int img1 = Image1.at<uchar>(referencialBlock.x + incrementX , referencialBlock.y + incrementY);
 			int img2 = Image2.at<uchar>(i, j);
-
-			int img1 = Image1.at<uchar>(referencialBlock.x + i - comparativeBlock.x, referencialBlock.y + j - comparativeBlock.y);
-
+			
 			int difference = std::pow((img2 - img1), 2);
-			evaluationFactor += difference;
+			SAD += difference;
 		}
 	}
 
-	return evaluationFactor;
+	return SAD;
+}
+
+Rect WindowSearch(Rect block, Rect blockSize, Rect searchWindowSize, Rect searchWindow, Mat image, Mat image2) {
+
+	int minorValue = std::numeric_limits<int>::max();
+	Rect matchingBlock;
+
+	for (int i = searchWindow.x; i < searchWindow.x + searchWindowSize.width; i++)
+	{
+		if (i >= 0 && i < image.rows)
+		{
+			for (int j = searchWindow.y; j < searchWindow.y + searchWindowSize.height; j++)
+			{
+				if (j >= 0 && j < image.cols)
+				{
+					int SADValue = EvaluateSAD(Rect(i, j, blockSize.width, blockSize.height), block, image, image2);
+
+					if (SADValue < minorValue) {
+						minorValue = SADValue;
+						matchingBlock = Rect(i, j, blockSize.width, blockSize.height);
+					}
+				}
+			}
+		}
+	}
+
+
+	std::cout << "Bloco analizado: x = " << block.x << ", y = " << block.y << endl;
+	std::cout << "Menor valor: " << minorValue << endl;
+	std::cout << "Best Matching: x = " << matchingBlock.x << ", y = " << matchingBlock.y << endl << endl;
+
+	return matchingBlock;
 }
 
 
-void FullSearch(Mat image, Mat image2, int blockSize, int searchWindowSize) {
+
+void FullSearch(Mat image, Mat image2, Rect blockSize, Rect searchWindowSize) {
 	list<Rect> blocks = list<Rect>();
 	
-	for (int line = 0; line < image.rows; line+=blockSize)
+	for (int line = 0; line < image.rows; line+=blockSize.width)
 	{
-		for (int column = 0; column < image.cols; column+=blockSize)
+		for (int column = 0; column < image.cols; column+=blockSize.height)
 		{
-			Rect p = Rect(line, column, blockSize, blockSize);
+			Rect p = Rect(line, column, blockSize.width, blockSize.height);
 			blocks.push_back(p);
 		}
 	}
 
 	list<Rect> matchingBlocks;
-
+	Mat resultImage = Mat(image.rows, image.cols, image.type());
 	for (list<Rect>::iterator block = blocks.begin(); block != blocks.end(); block++)
 	{
+		Point mean = Point(block->x + blockSize.width / 2, block->y + blockSize.height / 2);
+		Point searchWindowAxis = Point(mean.x - searchWindowSize.width / 2, mean.y - searchWindowSize.height / 2);		
+		Rect searchWindow = Rect(searchWindowAxis.x, searchWindowAxis.y, searchWindowSize.width, searchWindowSize.height);
 		
-		Point mean = Point(block->x + blockSize / 2, block->y + blockSize / 2);
+		thread AnalyzeWindow(WindowSearch, Rect(block->x, block->y, block->width, block->height), blockSize, searchWindowSize, searchWindow, image, image2);
+
+
+		//Mat ROIAnalyzedBlock = image(p);
+		//Mat ROIBestMatchingBlock = image(matchingBlock);
+		//Mat ROIOriginalImage = image2(p);
+
+		//OpenImage(ROIAnalyzedBlock, "Bloco analizado");
+		//OpenImage(ROIOriginalImage, "Imagem Original");
+		//OpenImage(ROIBestMatchingBlock, "Best-matching");
+		//waitKey(0);
+
+		//ROIBestMatchingBlock.copyTo(resultImage);
 		
-		Point searchWindowAxis = Point(mean.x - searchWindowSize / 2, mean.y - searchWindowSize / 2);
-		
-		Rect searchWindow = Rect(searchWindowAxis.x, searchWindowAxis.y, searchWindowSize, searchWindowSize);
-		
 
-		Rect matchingBlock;
-		int minorValue = std::numeric_limits<int>::max();
-		for (int i = searchWindow.x; i < searchWindow.x + searchWindowSize; i++)
-		{
-			if (i >= 0) 
-			{
-				for (int j = searchWindow.y; j < searchWindow.y + searchWindowSize; j++)
-				{
-					if (j >= 0) 
-					{
-						int evaluateValue = Evaluate(Rect(i, j, blockSize, blockSize), Rect(block->x, block->y, block->width, block->height), image, image2);
-						
-						if (evaluateValue < minorValue) {
-							minorValue = evaluateValue;
-							matchingBlock = Rect(i, j, blockSize, blockSize);
-						}
 
-						if (minorValue == 0)
-							break;
-					}
-				}
-			}
-		}
-		matchingBlocks.push_back(matchingBlock);
-		std::cout << "Bloco analizado: x = " << block->x << ", y = " << block->y << endl;
-		std::cout << "Menor valor: " << minorValue << endl;
-		std::cout << "Best Matching: x = " << matchingBlock.x << ", y = " << matchingBlock.y << endl <<endl;
 
-		Rect p = Rect(block->x, block->y, block->width, block->height);
-		Mat ROIAnalyzedBlock = image(p);
 
-		Mat ROIBestMatchingBlock = image2(matchingBlock);
-
-		OpenImage(ROIAnalyzedBlock, "Bloco analizado");
-		OpenImage(ROIBestMatchingBlock, "Best-matching");
-		waitKey(0);
-
-		std::cin.get();
+		//std::cin.get();
 
 
 
@@ -104,9 +116,7 @@ void FullSearch(Mat image, Mat image2, int blockSize, int searchWindowSize) {
 	}
 
 
-	//Montando as imagens lado a lado
-
-
+	
 
 
 
@@ -124,11 +134,14 @@ void FullSearch(Mat image, Mat image2, int blockSize, int searchWindowSize) {
 	//OpenImage(image, "White");
 }
 
+
+
+
 int main(int argc, char** argv) {
 	Mat image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 	Mat image2 = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
-
-	FullSearch(image, image2, 64, 128);
+	
+	FullSearch(image, image2, Rect(0, 0, 32, 32), Rect(0, 0, 128, 128));
 
 	OpenImage(image, "Imagem 1");
 	OpenImage(image2, "Imagem 2");
