d3bf3bfecd45b041f55a572dd76b6415668bc074
ShamelessCFG <simas.lucas@hotmail.com>
2016-10-06 11:21:51 -0300

    Mudanças no padrão de varredura / Avaliação

--- OpenCV/Source.cpp
+++ OpenCV/Source.cpp
@@ -2,16 +2,16 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
+#include <cmath>
 #include <list>
 #include <limits>
-#include <cmath>
 #include <thread>
 #include <stdlib.h>
 
 using namespace std;
 using namespace cv;
 
-list<Rect> matchingBlocks;
+list<pair<Rect, Rect>> matchingBlocks;
 bool isFinished = false;
 
 void OpenImage(Mat image, string windowName) {
@@ -19,7 +19,29 @@ void OpenImage(Mat image, string windowName) {
 	imshow(windowName, image);
 }	
 
-void ShowProgress(int count) {
+
+void MakeImage(Mat image){
+	int width = image.cols;
+	int height = image.rows;
+	
+	
+	Mat resultImage(width, height, image.type());
+
+	for (list<pair<Rect, Rect>>::iterator match = matchingBlocks.begin(); match != matchingBlocks.end(); match++)
+	{
+		Mat matchBlock = image(match->first);
+		Mat roi = resultImage(match->second);
+		matchBlock.copyTo(roi);
+	}
+
+	OpenImage(resultImage, "Final");
+	waitKey(0);
+
+}
+
+
+void ShowProgress(int count, Mat image) {
+	
 	while (!isFinished)
 	{
 		int x = matchingBlocks.size() * 100 / count;
@@ -28,32 +50,35 @@ void ShowProgress(int count) {
 
 		if (matchingBlocks.size() == count) {
 			isFinished = true;
+			//MakeImage(image);
 		}
 	}
 }
 
-int EvaluateSAD(Rect currentBlock, Rect referencialBlock, Mat Image1, Mat Image2) {
+int EvaluateMSE(Rect currentBlock, Rect referencialBlock, Mat Image1, Mat Image2) {
 	
-	int SAD = 0;
+	int MSE = 0;
 
-	for (int i = currentBlock.x; i < currentBlock.x + currentBlock.width; i++)
+	for (int y = currentBlock.y; y < currentBlock.y + currentBlock.height; y++)
 	{
-		for (int j = currentBlock.y; j < currentBlock.y + currentBlock.height; j++)
+		for (int x = currentBlock.x; x < currentBlock.x + currentBlock.width; x++)
 		{
-			int incrementX = i - currentBlock.x;
-			int incrementY = j - currentBlock.y;
-			int img1 = Image1.at<uchar>(referencialBlock.x + incrementX, referencialBlock.y + incrementY);
-			int img2 = Image2.at<uchar>(i, j);
+			int incrementX = x - currentBlock.x;
+			int incrementY = y - currentBlock.y;
 
+			Point referencialPoint(referencialBlock.x + incrementX, referencialBlock.y + incrementY);
+			Point currentPoint(x, y);
 
+			int img1 = Image1.at<uchar>(referencialPoint);
+			int img2 = Image2.at<uchar>(currentPoint);
 
-			int difference = std::pow((img2 - img1), 2);
-			SAD += difference;
+			int difference = pow(img2 - img1, 2);
+			MSE += difference;
 		}
 	}
 	
 
-	return SAD;
+	return (MSE / (currentBlock.width * currentBlock.height));
 }
 
 void WindowSearch(Rect block, Rect blockSize, Rect searchWindowSize, Rect searchWindow, Mat image, Mat image2) {
@@ -61,26 +86,34 @@ void WindowSearch(Rect block, Rect blockSize, Rect searchWindowSize, Rect search
 	int minorValue = std::numeric_limits<int>::max();
 	Rect matchingBlock;
 
-	for (int i = searchWindow.x; i < searchWindow.x + searchWindowSize.width; i++)
+	for (int y = searchWindow.y; y < searchWindow.y + searchWindowSize.height; y++)
 	{
-		if (i >= 0 && i < image.rows - block.height)
+		if (y >= 0 && y < image.cols - block.width - 1)
 		{
-			for (int j = searchWindow.y; j < searchWindow.y + searchWindowSize.height; j++)
+			for (int x = searchWindow.x; x < searchWindow.x + searchWindowSize.width; x++)
 			{
-				if (j >= 0 && j < image.cols - block.width)
+				if (x >= 0 && x < image.rows - block.height - 1)
 				{
-					int SADValue = EvaluateSAD(Rect(i, j, blockSize.width, blockSize.height), block, image, image2);
+					int SADValue = EvaluateMSE(Rect(x, y, blockSize.width, blockSize.height), block, image, image2);
 
 					if (SADValue < minorValue) {
 						minorValue = SADValue;
-						matchingBlock = Rect(i, j, blockSize.width, blockSize.height);
+						matchingBlock = Rect(x, y, blockSize.width, blockSize.height);
 					}
 				}
 			}
 		}
 	}
 
-	matchingBlocks.push_back(matchingBlock);
+	Mat matchingBlockROI = image(matchingBlock);
+	Mat blockROI = image2(block);
+	
+	//OpenImage(matchingBlockROI, "Matching Block");
+	//OpenImage(blockROI, "Original Block");
+	//waitKey(0);
+
+	pair<Rect, Rect> blockPair = make_pair(matchingBlock, block);
+	matchingBlocks.push_back(blockPair);
 }
 
 
@@ -88,22 +121,19 @@ void WindowSearch(Rect block, Rect blockSize, Rect searchWindowSize, Rect search
 void FullSearch(Mat image, Mat image2, Rect blockSize, Rect searchWindowSize) {
 	list<Rect> blocks = list<Rect>();
 	
-	for (int line = 0; line < image.rows; line+=blockSize.width)
+	for (int y = 0; y < image.rows - 1; y += blockSize.height)
 	{
-		for (int column = 0; column < image.cols; column+=blockSize.height)
+		for (int x = 0; x < image.cols - 1; x += blockSize.width)
 		{
-			Rect p = Rect(line, column, blockSize.width, blockSize.height);
+			Rect p = Rect(x, y, blockSize.width, blockSize.height);
 			blocks.push_back(p);
 		}
 	}
 
-	Mat resultImage = Mat(image.rows, image.cols, image.type());
-	
-
 	int count = blocks.size();
-	thread progressWatcher(ShowProgress, count);
+	thread progressWatcher(ShowProgress, count, image);
 	progressWatcher.detach();
-	
+
 	for (list<Rect>::iterator block = blocks.begin(); block != blocks.end(); block++)
 	{
 		Point mean = Point(block->x + blockSize.width / 2, block->y + blockSize.height / 2);
@@ -113,17 +143,13 @@ void FullSearch(Mat image, Mat image2, Rect blockSize, Rect searchWindowSize) {
 		thread AnalyzeWindow(WindowSearch, Rect(block->x, block->y, block->width, block->height), blockSize, searchWindowSize, searchWindow, image, image2);
 		AnalyzeWindow.detach();
 	}
-
 }
-
-
-
 
 int main(int argc, char** argv) {
 	Mat image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 	Mat image2 = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
 	
-	FullSearch(image, image2, Rect(0, 0, 32, 32), Rect(0, 0, 128, 128));
+	FullSearch(image, image2, Rect(0, 0, 16, 16), Rect(0, 0, 64, 64));
 
 	OpenImage(image, "Imagem 1");
 	OpenImage(image2, "Imagem 2");
