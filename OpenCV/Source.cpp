c8b9b81b024a9a4091616e39273ed1c7bda9698c
ShamelessCFG <simas.lucas@hotmail.com>
2016-10-28 03:28:52 -0300

    performance++
    Need test

--- OpenCV/Source.cpp
+++ OpenCV/Source.cpp
@@ -1,163 +1,238 @@
+//
+//#define IMAGE_WIDTH 480
+//#define IMAGE_HEIGHT 272
+//
+//#define BLOCK_WIDTH 16 
+//#define BLOCK_HEIGHT 16
+//
+//#define WINDOW_SEARCH_WIDTH 32
+//#define WINDOW_SEARCH_HEIGHT 32
+
+
+//
+#define IMAGE_WIDTH 1920
+#define IMAGE_HEIGHT 1088
+
+#define BLOCK_WIDTH 64 
+#define BLOCK_HEIGHT 64
+
+#define WINDOW_SEARCH_WIDTH 128
+#define WINDOW_SEARCH_HEIGHT 128
+
+
+
+
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include <iostream>
 #include <cmath>
 #include <list>
-#include <limits>
 #include <thread>
 #include <stdlib.h>
 
 using namespace std;
 using namespace cv;
 
-list<pair<Rect, Rect>> matchingBlocks;
-bool isFinished = false;
-
-void OpenImage(Mat image, string windowName) {
-	namedWindow(windowName, WINDOW_AUTOSIZE);
-	imshow(windowName, image);
-}	
 
 
-void MakeImage(Mat image, Mat image2){
-	int width = image.cols;
-	int height = image.rows;
-	
-	
-	//Mat resultImage(width, height, image.type());
+int img1[IMAGE_HEIGHT][IMAGE_WIDTH], img2[IMAGE_HEIGHT][IMAGE_WIDTH], result[IMAGE_HEIGHT][IMAGE_WIDTH];
+int total = IMAGE_HEIGHT / BLOCK_HEIGHT * IMAGE_WIDTH / BLOCK_WIDTH, processados = 0;
 
-	for (list<pair<Rect, Rect>>::iterator match = matchingBlocks.begin(); match != matchingBlocks.end(); match++)
-	{
-		Mat matchBlock = image(match->first);
-		Mat roi = image2(match->second);
-		matchBlock.copyTo(roi);
-	}
+void OpenImage(Mat image, string windowName);
 
-	imwrite("result.jpg", image2);
+void MakeImage(Mat image, Mat image2);
+
+void ShowProgress(/*int count, Mat image, Mat image2*/);
+
+int EvaluateMSE(/*int img1[IMAGE_HEIGHT][IMAGE_WIDTH], int img2[IMAGE_HEIGHT][IMAGE_WIDTH],*/ int row, int column, int cx, int cy);
+
+void WindowSearch(/*int img1[IMAGE_HEIGHT][IMAGE_WIDTH], int img2[IMAGE_HEIGHT][IMAGE_WIDTH], int result[IMAGE_HEIGHT][IMAGE_WIDTH],*/ int row, int column, int searchWindowX, int searchWindowY);
+
+void FullSearch(/*int img1[IMAGE_HEIGHT][IMAGE_WIDTH], int img2[IMAGE_HEIGHT][IMAGE_WIDTH], int result[IMAGE_HEIGHT][IMAGE_WIDTH]*/);
+
+void GetGrayscaleMatrix(Mat image, int img[][IMAGE_WIDTH]);
+
+void AddToResult(int row, int column, int bestx, int besty);
+
+int main(/*int argc, char** argv*/) 
+{
+
+	Mat image = imread("C:\\blue_sky\\0mini.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat image2 = imread("C:\\blue_sky\\1mini.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 
 
-	OpenImage(image2, "Final");
-	waitKey(0);
+
+	GetGrayscaleMatrix(image, img1);
+	GetGrayscaleMatrix(image2, img2);
+
+
+	FullSearch(/*img1, img2, result*/);
 
 }
 
 
-void ShowProgress(int count, Mat image, Mat image2) {
-	
-	while (!isFinished)
-	{
-		int x = matchingBlocks.size() * 100 / count;
-		system("cls");
-		std::cout << x << "%";
+void FullSearch(/*int img1[IMAGE_HEIGHT][IMAGE_WIDTH], int img2[IMAGE_HEIGHT][IMAGE_WIDTH], int result[IMAGE_HEIGHT][IMAGE_WIDTH]*/) {
 
-		if (matchingBlocks.size() == count) {
-			isFinished = true;
-			MakeImage(image, image2);
+
+
+
+
+
+	for (int y = 0; y < IMAGE_HEIGHT; y += BLOCK_HEIGHT)
+	{
+		for (int x = 0; x < IMAGE_WIDTH; x += BLOCK_WIDTH)
+		{
+
+			//WindowSearch(x, y, ((x + BLOCK_WIDTH / 2) - WINDOW_SEARCH_WIDTH / 2), ((y + BLOCK_HEIGHT / 2) - WINDOW_SEARCH_HEIGHT / 2));
+			thread AnalyzeWindow(WindowSearch, /*img1, img2, result,*/ x, y, ((x + BLOCK_WIDTH / 2) - WINDOW_SEARCH_WIDTH / 2), ((y + BLOCK_HEIGHT / 2) - WINDOW_SEARCH_HEIGHT / 2));
+			AnalyzeWindow.detach();
+			//thread Watcher(ShowProgress);
+			//Watcher.join();
 		}
 	}
+
+
+
 }
 
-int EvaluateMSE(Rect currentBlock, Rect referencialBlock, Mat Image1, Mat Image2) {
-	
-	int SAD = 0;
+void WindowSearch(/*int img1[IMAGE_HEIGHT][IMAGE_WIDTH], int img2[IMAGE_HEIGHT][IMAGE_WIDTH], int result[IMAGE_HEIGHT][IMAGE_WIDTH],*/ int row, int column, int searchWindowX, int searchWindowY) {
 
-	for (int y = currentBlock.y; y < currentBlock.y + currentBlock.height; y++)
+	int minorValue = 99999999;
+	int bestx, besty;
+
+	for (int y = searchWindowY; y < searchWindowY + WINDOW_SEARCH_HEIGHT; y++)
 	{
-		for (int x = currentBlock.x; x < currentBlock.x + currentBlock.width; x++)
+		if (y >= 0 && y < IMAGE_HEIGHT - BLOCK_HEIGHT)
 		{
-			int incrementX = x - currentBlock.x;
-			int incrementY = y - currentBlock.y;
-
-			Point referencialPoint(referencialBlock.x + incrementX, referencialBlock.y + incrementY);
-			Point currentPoint(x, y);
-
-			int img1 = Image1.at<uchar>(referencialPoint);
-			int img2 = Image2.at<uchar>(currentPoint);
-
-			int difference = pow(img2 - img1, 2);
-			SAD += difference;
-		}
-	}
-	
-
-	return (SAD);
-}
-
-void WindowSearch(Rect block, Rect searchWindow, Mat image, Mat image2) {
-
-	int minorValue = std::numeric_limits<int>::max();
-	Rect matchingBlock;
-
-	for (int y = searchWindow.y; y < searchWindow.y + searchWindow.height; y++)
-	{
-		if (y >= 0 && y < image.rows - block.height)
-		{
-			for (int x = searchWindow.x; x < searchWindow.x + searchWindow.width; x++)
+			for (int x = searchWindowX; x < searchWindowY + WINDOW_SEARCH_WIDTH; x++)
 			{
-				if (x >= 0 && x < image.cols - block.width)
+				if (x >= 0 && x < IMAGE_WIDTH - BLOCK_WIDTH)
 				{
-					int SADValue = EvaluateMSE(Rect(x, y, block.width, block.height), block, image, image2);
+					int SADValue = EvaluateMSE(/*img1, img2,*/ row, column, x, y);
 
-					if (SADValue < minorValue) {
+					if (SADValue < minorValue) 
+					{
 						minorValue = SADValue;
-						matchingBlock = Rect(x, y, block.width, block.height);
+						bestx = x;
+						besty = y;
 					}
 				}
 			}
 		}
 	}
 
-	Mat matchingBlockROI = image(matchingBlock);
-	Mat blockROI = image2(block);
-	
-	//OpenImage(matchingBlockROI, "Matching Block");
-	//OpenImage(blockROI, "Original Block");
-	//waitKey(0);
+	//AddToResult(row, column, bestx, besty);
+	cout << ++processados << "/" << total<< endl;
+	//++processados;
+	//ShowProgress();
+}
 
-	pair<Rect, Rect> blockPair = make_pair(matchingBlock, block);
-	matchingBlocks.push_back(blockPair);
+void AddToResult(int row, int column, int bestx, int besty) {
+	for (int x = row; x < row + BLOCK_HEIGHT; x++)
+	{
+		for (int y = column; y < BLOCK_WIDTH; y++)
+		{
+			result[row + x][column + y] = img1[bestx + x][besty + y];
+		}
+	}
 }
 
 
+void OpenImage(Mat image, string windowName) {
+	namedWindow(windowName, WINDOW_AUTOSIZE);
+	imshow(windowName, image);
+}
 
-void FullSearch(Mat image, Mat image2, Rect blockSize, Rect searchWindowSize) {
-	list<Rect> blocks = list<Rect>();
-	
-	for (int y = 0; y < image.rows; y += blockSize.height)
+
+//void MakeImage(Mat image, Mat image2) {
+//	int width = image.cols;
+//	int height = image.rows;
+//
+//
+//	//Mat resultImage(width, height, image.type());
+//
+//	for (list<pair<Rect, Rect>>::iterator match = matchingBlocks.begin(); match != matchingBlocks.end(); match++)
+//	{
+//		Mat matchBlock = image(match->first);
+//		Mat roi = image2(match->second);
+//		matchBlock.copyTo(roi);
+//	}
+//
+//	imwrite("result.jpg", image2);
+//
+//
+//	OpenImage(image2, "Final");
+//	waitKey(0);
+//
+//}
+
+
+void ShowProgress() {
+
+	while (total != processados)
 	{
-		for (int x = 0; x < image.cols; x += blockSize.width)
+		int x = processados * 100 / total;
+		system("cls");
+		std::cout << x << "%";
+	}
+}
+
+
+//#define IMAGE_WIDTH 1920
+//#define IMAGE_HEIGHT 1088
+//
+//#define BLOCK_WIDTH 64 
+//#define BLOCK_HEIGHT 64 
+//
+//#define WINDOW_SEARCH_WIDTH 128
+//#define WINDOW_SEARCH_HEIGHT 128
+int EvaluateMSE(/*int img1[IMAGE_HEIGHT][IMAGE_WIDTH], int img2[IMAGE_HEIGHT][IMAGE_WIDTH], */int row, int column, int cx, int cy) {
+
+	int SAD = 0;
+
+	for (int y = column; y < column + BLOCK_WIDTH; y++)
+	{
+		for (int x = row; x < row + BLOCK_HEIGHT; x++)
 		{
-			Rect p = Rect(x, y, blockSize.width, blockSize.height);
-			blocks.push_back(p);
+			int incrementX = x - row;
+			int incrementY = y - column;
+
+
+			//Point referencialPoint(cx + incrementX, referencialBlock.y + incrementY);
+			//Point currentPoint(x, y);
+
+			//int img1 = Image1.at<uchar>(referencialPoint);
+			//int img2 = Image2.at<uchar>(currentPoint);
+
+
+
+			int valueimg1 = img1[cx + incrementX][cy + incrementY];
+			int valueimg2 = img2[x][y];
+
+
+			int difference = pow(valueimg2 - valueimg1, 2);
+			SAD += difference;
 		}
 	}
 
-	int count = blocks.size();
-	thread progressWatcher(ShowProgress, count, image, image2);
-	progressWatcher.detach();
 
-	for (list<Rect>::iterator block = blocks.begin(); block != blocks.end(); block++)
+	return (SAD);
+}
+
+
+
+
+
+
+
+void GetGrayscaleMatrix(Mat image, int img[][IMAGE_WIDTH]) {
+
+	for (int row = 0; row < image.rows; row++)
 	{
-		Point mean = Point(block->x + blockSize.width / 2, block->y + blockSize.height / 2);
-		Point searchWindowAxis = Point(mean.x - searchWindowSize.width / 2, mean.y - searchWindowSize.height / 2);		
-		Rect searchWindow = Rect(searchWindowAxis.x, searchWindowAxis.y, searchWindowSize.width, searchWindowSize.height);
-
-		thread AnalyzeWindow(WindowSearch, Rect(block->x, block->y, block->width, block->height), searchWindow, image, image2);
-		AnalyzeWindow.detach();
+		for (int column = 0; column < image.cols; column++)
+		{
+			img[row][column] = (int)image.at<uchar>(Point(column, row));
+		}
 	}
 }
-
-int main(int argc, char** argv) {
-	Mat image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-	Mat image2 = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
-	
-	FullSearch(image, image2, Rect(0, 0, 64, 64), Rect(0, 0, 128, 128));
-
-	OpenImage(image, "Imagem 1");
-	//OpenImage(image2, "Imagem 2");
-
-	waitKey(0);
-}
-
-
